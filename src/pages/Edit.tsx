453c8e26811a065e8f4d84839eb6fdbc33fea5e6
enthusiast <rajankannaujiya2034@gmail.com>
2024-09-06 15:57:23 +0530

    first commit1

--- src/pages/Edit.tsx
+++ src/pages/Edit.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 import EditComp from '../components/EditComp'
 import Appbar from '../components/Appbar'
 
--- src/pages/View.tsx
+++ src/pages/View.tsx
@@ -1,4 +1,4 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
+
 import { useEffect, useState } from "react"
 import Appbar from "../components/Appbar"
 import ViewComp from '../components/ViewComp'
